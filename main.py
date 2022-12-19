91f374f21dcf664bd9087982b1d5e1234701fa35
ajajw <111954733+ajajw@users.noreply.github.com>
2022-12-18 23:19:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -70,6 +70,7 @@ async def my_event_handler(m):
         return
     bin_json =  bin.json()
     addr = real_random_address()
+    fullinfo = f"{cc}|{mes}|{ano}|{cvv}|{names.get_full_name()}|{addr['address1']}|{addr['city']}|{addr['state']}|{addr['postalCode']}|{phone()}|dob: {datetime.strftime(datetime(random.randint(1960, 2005), random.randint(1, 12),random.randint(1, 28), ), '%Y-%m-%d')}|United States Of America"
     text = f"""
 💵 𝘾𝘾:  `{cc}|{mes}|{ano}|{cvv}`
 
